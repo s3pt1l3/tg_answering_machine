6b8585c0f8e63be4f783be10168e790c7409d704
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-06-20 11:34:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from typing import Text
 from telethon import TelegramClient, sync, events
 import json
 
+# Opening the config file
 with open('config.json', 'r', encoding='utf-8') as config_file:
     config = json.load(config_file)
+
+# Getting api data
 api_id = config['login']['api_id']
 api_hash = config['login']['api_hash']
+
+# Telegram client authorization
 with TelegramClient('session', api_id, api_hash) as client:
     @client.on(events.NewMessage())
     async def handler(event):
+        # Message handler
         for key in config['keys']:
             if key.lower() in event.message.raw_text.lower():
                 await event.reply(config['message'])
